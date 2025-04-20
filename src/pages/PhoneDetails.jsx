c406bb714c28e84982ce7cef2124db2c1003f528
Helal366 <helal56876@gmail.com>
2025-04-20 06:42:57 +0600

    single phone data updated

--- src/pages/PhoneDetails.jsx
+++ src/pages/PhoneDetails.jsx
@@ -3,28 +3,37 @@ import { useLoaderData, useParams } from "react-router";
 import { MdBookmarkAdd } from "react-icons/md";
 import { MdShoppingCartCheckout } from "react-icons/md";
 import Button from "../components/Button";
-import { addCartToLS, addFavoriteToLocalStorage, getCartFromLS } from "../utilities/localStorage";
+import {
+  addCartToLS,
+  addFavoriteToLocalStorage,
+  getCartFromLS,
+} from "../utilities/localStorage";
 import { CartContext } from "../useContexts/CartContext";
 
 const PhoneDetails = () => {
   const { jhankar } = useParams();
   const data = useLoaderData();
   const idInt = parseInt(jhankar);
-  const {setCart}=useContext(CartContext)
+  const { setCart } = useContext(CartContext);
   const selectedPhone = data.find((phone) => phone.id === idInt);
-  const { name, price, brand, description, camera_info, image, model, storage } =
-    selectedPhone;
+  const {
+    name,
+    price,
+    brand,
+    description,
+    camera_info,
+    image,
+    model,
+    storage,
+  } = selectedPhone;
   console.log(selectedPhone);
-const handleFavorite=()=>{
-    addFavoriteToLocalStorage(selectedPhone)
-};
-const handleCartAdd=()=>{
-  addCartToLS(selectedPhone);
-  setCart(getCartFromLS);
-};
-
-  const priceData= Object.entries(price);
-  const storageData= Object.entries(storage);
+  const handleFavorite = () => {
+    addFavoriteToLocalStorage(selectedPhone);
+  };
+  const handleCartAdd = () => {
+    addCartToLS(selectedPhone);
+    setCart(getCartFromLS);
+  };
 
   return (
     <>
@@ -32,27 +41,57 @@ const handleCartAdd=()=>{
         <img
           src={image}
           alt={name}
-          className="w-full mx-auto md:max-w-md mb-10"
+          className="w-full mx-auto md:w-auto mb-10"
         />
         <div className="flex justify-between">
           <h1 className="text-5xl lg:text-6xl font-thin mb-8">{name}</h1>
           <div className="space-x-4">
-            <Button onClick={handleCartAdd} label={<MdShoppingCartCheckout />}></Button>
+            <Button
+              onClick={handleCartAdd}
+              label={<MdShoppingCartCheckout />}
+            ></Button>
             <Button onClick={handleFavorite} label={<MdBookmarkAdd />}></Button>
           </div>
         </div>
         {/* Deatils */}
-        <div>
-          <p>{brand} {model} </p>
-          <p> </p>
-          <p>{camera_info} </p>
-          <p>{description} </p>
-          {/* <p>
-            Price: {priceData[0][0]} : {priceData[0][1]} and {priceData[1][0]} : {priceData[1][1]}
-          </p>
+
+        <div className="flex flex-col gap-4">
+          <h2 className="text-3xl lg:text-4xl font-thin">Details:</h2>
+          {/* brand */}
           <p>
-            Storage & RAM: {storageData[0][1]} and {storageData[1][1]}
-          </p> */}
+            <b>Brand: </b> {brand}{" "}
+          </p>
+          {/* model */}
+          <p>
+            <b>Model: </b> {model}
+          </p>
+
+          
+          {/* storage */}
+          <div className="flex gap-2">
+            <p className="font-bold">Storage: </p>
+            <div>
+              {storage.map((item, i) => (
+                <p key={i}>{item} </p>
+              ))}
+            </div>
+          </div>
+              {/* price */}
+          <div className="flex gap-2">
+            <p className="font-bold">Price: </p>
+            <div>
+              {Object.entries(price).map(([storage, price], i) => (
+                <p key={i}>
+                  <span>{storage}: </span> <span>{price} </span>
+                </p>
+              ))}
+            </div>
+          </div>
+
+              {/* camera info */}
+              <p><b>Camera info: </b> {camera_info} </p>
+              {/* description */}
+              <p><b>Description: </b> {description} </p>
         </div>
       </div>
     </>
